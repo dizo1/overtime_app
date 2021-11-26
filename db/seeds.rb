bba169423d030d3827aa1dfdce5df2f1601cc829
Dralega Denis <dizomuganda@ymail.com>
2021-11-26 18:07:55 +0300

    more changes to user schema file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |post|
+    Post.create!(date: Date.today, rationale: "#{post} rationale content")
+end
+
+puts "50 posts have been created"
